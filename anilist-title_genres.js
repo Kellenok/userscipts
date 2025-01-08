695a4136210fda1ecb7396a0dafd107cd27ad743
Kellen <kputuhuk@gmail.com>
2025-01-08 21:22:05 +0300

    Update anilist-title_genres.js

--- anilist-title_genres.js
+++ anilist-title_genres.js
@@ -6,8 +6,6 @@
 // @author        kln (t.me/kln_lzt)
 // @homepageURL   https://github.com/Kellenok/userscipts/
 // @supportURL    https://github.com/Kellenok/userscipts/issues
-// @downloadURL   https://github.com/Kellenok/userscipts/blob/main/anilist-title_genres.js
-// @updateURL     https://github.com/Kellenok/userscipts/blob/main/anilist-title_genres.js
 // @match         https://anilist.co/anime/*
 // @match         https://anilist.co/manga/*
 // @grant         none
